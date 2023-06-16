2d8d64c5905a90f0f1fc92cd99cbb2598e5e2ea7
KT <132851569+kumih0@users.noreply.github.com>
2023-06-15 23:20:30 -0500

    using code from previous activities in unit

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,13 @@
+// TODO: Import express
 const express = require('express');
-const path = require('path');
+// TODO: Import modules for notes
+const notesRouter = require('./notes');
 
-// Import the feedback router
-const api = require('./routes/index');
-
-const PORT = 3001;
-
+// TODO: Create app variable to create an instance of express()
 const app = express();
 
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// TODO: Use our routes
+app.use('/notes', notesRouter);
 
-// Middleware to serve up static assets from the public folder
-app.use(express.static('public'));
-
-// Send all the requests that begin with /api to the index.js in the routes folder
-app.use('/api', api);
-
-// This view route is a GET route for the homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// This view route is a GET route for the feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+// TODO: Export app
+module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,33 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+
+// Import the feedback router
+const api = require('./routes/index');
+
+const PORT = 3001;
+
 const app = express();
-const port = 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Middleware to serve up static assets from the public folder
+app.use(express.static('public'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// Send all the requests that begin with /api to the index.js in the routes folder
+app.use('/api', api);
+
+// This view route is a GET route for the homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// This view route is a GET route for the notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
