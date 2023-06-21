c76a4fa1e17f1fe8fdd2a380e1ee6a7f632e13e3
KT <132851569+kumih0@users.noreply.github.com>
2023-06-21 17:41:33 -0500

    i fxckeed up index.html so so stupid

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,14 +5,14 @@ const path = require('path');
 
 
 //GET /notes -> sends note.html file
-router.get('/notes', (req, res) => {
+router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'), (err) => err ? console.log(err, 'u broke it no notes') : console.log('u done it'));
 });
 
 //GET * -> sends index.html file
-// router.get('////', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/index.html'), (err) => err ? console.log(err, 'u broke it u dummy') : console.log('u did it'));
-// });
+router.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'), (err) => err ? console.log(err, 'u broke it u dummy') : console.log('u did it'));
+});
 
 //export html files
 module.exports = router;
