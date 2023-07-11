b4eb1713f2436cef4d7f49fa489b568ed764c343
KT <132851569+kumih0@users.noreply.github.com>
2023-07-11 13:17:27 -0500

    correcting routes, method call, and cleaning up

--- helpers/store.js
+++ helpers/store.js
@@ -3,20 +3,24 @@
 const fs = require('fs');
 const util = require('util');
 const { v4: uuidv4 } = require('uuid');
-// uuidv4();<- you call it here like this one
-
 // Promise version of fs.readFile/writefile to make them async
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 class Store {
+  //read method
   read(){
     return readFile('db/db.json', 'utf-8');
-  }
+  };
+
+  //write method
   write(note){
     return writeFile('db/db.json', JSON.stringify(note), (err) => err ? console.log(err) : console.info(`Note written to db.json`));
-  }
+  };
+
+  //add note method that contains get notes method
   addNote(note){
+    //destructuring note obj
     const { title, text } = note;
     const newNote = {
       title,
@@ -24,6 +28,7 @@ class Store {
       note_id: uuidv4(),//<-calling the uuid npm boi to run when that new note obj is made
     }
     console.log(newNote);
+    
     //getnotes method> grabbing dbjson obj and push new note into array, stringfy the new array (done inside write method) and put it back w write method
     const getNotes = () => {
       return JSON.parse(read());
@@ -31,7 +36,8 @@ class Store {
     this.write(getNotes.push(newNote));
     //method returns newnote obj so it can be passed into the res part of post method in apiroutes
     return newNote;
-  }
+  };
+
   // deleteNote
 }
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,4 @@
-// const express = require('express');
 const router = require('express').Router();
-//Store.addNote(req.body)
 // import that sweet sweet store class
 const Store = require('../helpers/store');
 
@@ -8,32 +6,20 @@ const Store = require('../helpers/store');
  router.get('/notes', (req, res) => {
   console.info(`${req.method} request received for notes`);
 //using readfile method from store export to read db.json file and spit out the json parsed data
-  Store.read().then((data) => res.json(JSON.parse(data)));
-  // readFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  new Store.read().then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route for saving note
 router.post('/notes', (req, res) => {
-//   // Log that a POST request was received
+// Log that a POST request was received
   console.info(`${req.method} request received to save note`);
 
-//   // Destructuring assignment for the items in req.body
-//   const {  noteTitle, noteText } = req.body;
-
-//   // If all the required properties are present
-//   if (noteTitle && noteText) {
-//     // Variable for the object we will save
-//     const newNote = {
-//       title,
-//       text,
-//       notes_id: uuid(),
-//     };
-
-//     readAndAppend(newNote, './db/db.json');
+  // Destructuring assignment for the items in req.body
 if (req.body.title || req.body.text) {
     //saving newnote obj created in store class addnote method to variable. addnote method writes in db.json file but returns the newnote obj BOOM BABY
-    const newNote =  Store.addNote(req.body);
+    const newNote =  new Store.addNote(req.body);
 
+    //adding to json file w response obj as 
     const response = {
       status: 'u done it',
       body: newNote,
