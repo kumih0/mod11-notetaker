a846dd7e31ec3cbe870e8ac41c02d46f62011379
KT <132851569+kumih0@users.noreply.github.com>
2023-07-19 08:27:20 -0500

    forgot async

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,15 +17,15 @@ router.get('/notes', async (req, res) => {
 });
 
 // POST Route for saving note
-router.post('/notes', (req, res) => {
+router.post('/notes', async (req, res) => {
 // Log that a POST request was received
   console.info(`${req.method} request received to save note`);
 
   // Destructuring assignment for the items in req.body
 if (req.body.title || req.body.text) {
     //saving newnote obj created in store class addnote method to variable, addnote method writes in db.json file
-    const newNote =  new Store();
-    newNote.addNote(req.body);
+    const newNote = new Store();
+    await newNote.addNote(req.body);
 
     //adding to json file w response obj
     const response = {
